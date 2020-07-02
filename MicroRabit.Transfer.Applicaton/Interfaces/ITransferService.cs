780720df613f1851ecbd0e24c9e6f39caa9d2ef2
suppchai <kalmro4@gmail.com>
2020-07-02 14:14:07 +0700

    Refactor RabbitMQ Bus for Dependency Injection for parameterless constructors in our event handlers

---
+++ MicroRabit.Transfer.Applicaton/Interfaces/ITransferService.cs
@@ -0,0 +1,12 @@
+﻿using MicroRabbit.Transfer.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabit.Transfer.Applicaton.Interfaces
+{
+    public interface ITransferService
+    {
+        IEnumerable<TransferLog> GetTransfarLogs();
+    }
+}
--- database.sql
+++
@@ -1,5 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [Id]
-      ,[AccontType]
-      ,[AccountBalance]
-  FROM [BankingDB].[dbo].[Accounts]
